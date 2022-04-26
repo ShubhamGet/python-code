b41a0c7698f4ef4c4f55c2065e55942389a5a711
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2022-04-26 07:33:58 -0700

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,15 @@
+# How to add Data in empty dictionary
+'''
+user_info={}
+user_info['Name']='Maan'
+user_info['Age']='22'
+user_info['Fav_movie']='Don2'
+print(user_info)
+print(user_info['Name'])
+print("\n")
+print("Key and value foramt:\n")
+for key,value in user_info.items():
+    print(f"{key}:{value}")
+print(user_info['Fav_movie'])
+'''
+
